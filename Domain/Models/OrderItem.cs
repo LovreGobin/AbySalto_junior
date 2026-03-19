94dc17dbdd8b9d13f7066c18825948ef0ebc9736
LovreGobin <lovre.gobin0@gmail.com>
2026-03-19 21:05:46 +0100

    Added Domain Models

---
+++ Domain/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+    }
+}
